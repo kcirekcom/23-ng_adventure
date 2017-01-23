9c6d2ade372dd7ebf956c9b21172f3b610b15b7d
kcirekcom <erick.f.mock@gmail.com>
2017-01-23 09:35:10 -0800

    required game pad component into entry file

--- app/entry.js
+++ app/entry.js
@@ -4,6 +4,10 @@ const angular = require('angular');
 
 angular.module('ngAdventure', []);
 
+// services
 require('./service/map-service.js');
 require('./service/select-box-service.js');
 require('./service/player-service.js');
+
+//components
+require('./component/game-pad/game-pad.js');
